6c897397d92f21cd099387528ad2b968a6e6ec67
AALETI SANTHOSH KUMAR <105435082+SANTHOSH2945@users.noreply.github.com>
2026-05-20 21:23:48 +0530

    Update VerifyOtp.jsx

--- src/pages/VerifyOtp.jsx
+++ src/pages/VerifyOtp.jsx
@@ -1,1 +1,44 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { supabase } from '../supabase'
 
+function VerifyOtp() {
+
+  const [otp, setOtp] = useState('')
+  const navigate = useNavigate()
+
+  const mobile = localStorage.getItem('mobile')
+
+  const verifyOtp = async () => {
+
+    const { error } = await supabase.auth.verifyOtp({
+      phone: '+91' + mobile,
+      token: otp,
+      type: 'sms'
+    })
+
+    if (!error) {
+      navigate('/dashboard')
+    }
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+
+      <h1>Verify OTP</h1>
+
+      <input
+        placeholder="Enter OTP"
+        value={otp}
+        onChange={(e) => setOtp(e.target.value)}
+      />
+
+      <button onClick={verifyOtp}>
+        Verify
+      </button>
+
+    </div>
+  )
+}
+
+export default VerifyOtp
