103df9c5b11e094cfc9a4b55a739e69a2ab55b98
AALETI SANTHOSH KUMAR <105435082+SANTHOSH2945@users.noreply.github.com>
2026-05-20 21:22:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Login from './pages/Login'
+import VerifyOtp from './pages/VerifyOtp'
+import Dashboard from './pages/Dashboard'
+import Quiz from './pages/Quiz'
+import Result from './pages/Result'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/verify" element={<VerifyOtp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
