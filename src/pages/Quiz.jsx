2fba92afae264f627c650e148db767ca0d654f42
AALETI SANTHOSH KUMAR <105435082+SANTHOSH2945@users.noreply.github.com>
2026-05-20 21:24:54 +0530

    Update Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,1 +1,53 @@
+import { useEffect, useState } from 'react'
+              onClick={() =>
+                selectAnswer(q.id, 'A')
+              }
+            >
+              {q.option_a}
+            </button>
 
+            <br />
+
+            <button
+              onClick={() =>
+                selectAnswer(q.id, 'B')
+              }
+            >
+              {q.option_b}
+            </button>
+
+            <br />
+
+            <button
+              onClick={() =>
+                selectAnswer(q.id, 'C')
+              }
+            >
+              {q.option_c}
+            </button>
+
+            <br />
+
+            <button
+              onClick={() =>
+                selectAnswer(q.id, 'D')
+              }
+            >
+              {q.option_d}
+            </button>
+
+            <hr />
+
+          </div>
+        ))
+      }
+
+      <button onClick={submitQuiz}>
+        Submit Quiz
+      </button>
+
+    </div>
+  )
+}
+
+export default Quiz
