d49f6770fe53f002be3cc1d52276b9ec94937664
AALETI SANTHOSH KUMAR <105435082+SANTHOSH2945@users.noreply.github.com>
2026-05-20 21:24:13 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,1 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 
+function Dashboard() {
+
+  const navigate = useNavigate()
+
+  return (
+    <div style={{ padding: 20 }}>
+
+      <h1>Dashboard</h1>
+
+      <button onClick={() => navigate('/quiz')}>
+        Start Daily Quiz
+      </button>
+
+    </div>
+  )
+}
+
+export default Dashboard
