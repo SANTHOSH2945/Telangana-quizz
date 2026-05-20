1634aaa708af7692bede8991a4a6b9068d5685df
AALETI SANTHOSH KUMAR <105435082+SANTHOSH2945@users.noreply.github.com>
2026-05-20 21:25:12 +0530

    Update Result.jsx

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,1 +1,24 @@
+function Result() {
 
+  const score = localStorage.getItem('score')
+
+  return (
+    <div style={{ padding: 20 }}>
+
+      <h1>Quiz Result</h1>
+
+      <h2>
+        Your Score: {score}/20
+      </h2>
+
+      {
+        score < 10
+          ? <p>Improve Current Affairs Preparation</p>
+          : <p>Good Performance</p>
+      }
+
+    </div>
+  )
+}
+
+export default Result
